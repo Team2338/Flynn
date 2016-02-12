a59c3f19f62a26bdd4590c39f5ef660d1ed8ec9e
TheShah1999 <armaanshah1999@gmail.com>
2016-02-11 19:23:47 -0600

    shooterAngle command

--- src/org/usfirst/frc/team2338/robot/Robot.java
+++ src/org/usfirst/frc/team2338/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends IterativeRobot {
         SmartDashboard.putData("Auto mode", chooser);
         
         teleOpCommand = new TankDrive();
+        teleOpCommand = new ShooterAngleChange();
     }
 	
 
